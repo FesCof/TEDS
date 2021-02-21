c621efb880fd4b8872cf151697d9d7a4323e1e5f
FesCof <79240225+FesCof@users.noreply.github.com>
2021-02-21 13:03:28 +0800

    Update personal.php

--- php/personal.php
+++ php/personal.php
@@ -1,16 +1,14 @@
 <?php
 $id= implode("", $_GET);
-$servername = "172.27.16.3";
-$username = "guest";
-$password = "lee01020304";
-// 创建连接
+$servername = "servername";
+$username = "username";
+$password = "password";
 $conn = mysqli_connect($servername, $username, $password);
 $conn->set_charset("utf8");
 ini_set("date.timezone", "PRC");
 
-// 检测连接
 if ($conn->connect_error) {
-    die("连接失败: " . $conn->connect_error);
+    die("error: " . $conn->connect_error);
 } 
 $sql= "select * from wiki.character where id= ".$id;
 $res=$conn->query($sql);
