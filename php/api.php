b1a9dacee739357ee06ff571d7b22628d15b8ac5
FesCof <79240225+FesCof@users.noreply.github.com>
2021-02-21 13:02:57 +0800

    Update api.php

--- php/api.php
+++ php/api.php
@@ -2,12 +2,11 @@
 $servername = "servername";
 $username = "username";
 $password = "password";
-// ´´½¨Á¬½Ó
+
 $conn = mysqli_connect($servername, $username, $password);
 $conn->set_charset("utf8");
 ini_set("date.timezone", "PRC");
 
-// ¼ì²âÁ¬½Ó
 if ($conn->connect_error) {
     die("error: " . $conn->connect_error);
 } 
